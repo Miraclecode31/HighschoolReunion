7d7907b36194fcb0d77327b4d38a93c17af11c79
Miraclecode31 <mirmathis23@gmail.com>
2025-03-27 20:04:37 -0400

    backend/mongoconnected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 3000;
+
+const MONGO_URI = process.env.MONGO_URI;
+
+
+mongoose.connect(MONGO_URI) 
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 // Simple Route
 app.get('/', (req, res) => {
